0748ad68abcb585f0830f9184836f758cc4d4f36
Mrtckr008 <mrtckr008@gmail.com>
2020-06-10 18:15:32 +0300

    * BaseActivity and BaseFragment added.

--- app/src/main/java/com/huawei/references/hinotes/ui/todolist/ToDoListsViewModel.kt
+++ app/src/main/java/com/huawei/references/hinotes/ui/todolist/ToDoListsViewModel.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class ToDoListViewModel : ViewModel() {
+class ToDoListsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is to-do list fragment"
