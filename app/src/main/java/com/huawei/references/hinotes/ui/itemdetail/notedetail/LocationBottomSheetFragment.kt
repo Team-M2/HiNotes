3e872a9da062685ff43ee5d69410e6521e3ced12
m00565027 <murat.cakir1@huawei.com>
2020-07-14 10:53:01 +0300

    Location fragment set to BaseFragment.

--- app/src/main/java/com/huawei/references/hinotes/ui/itemdetail/notedetail/LocationBottomSheetFragment.kt
+++ app/src/main/java/com/huawei/references/hinotes/ui/itemdetail/notedetail/LocationBottomSheetFragment.kt
@@ -1,15 +1,20 @@
 package com.huawei.references.hinotes.ui.itemdetail.notedetail
 
+import android.Manifest
 import android.Manifest.permission
+import android.app.Activity
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.annotation.RequiresPermission
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.huawei.hms.location.LocationServices
 import com.huawei.hms.maps.CameraUpdateFactory
 import com.huawei.hms.maps.HuaweiMap
 import com.huawei.hms.maps.MapView
@@ -24,10 +29,12 @@ import com.huawei.hms.site.api.SearchServiceFactory
 import com.huawei.hms.site.api.model.*
 import com.huawei.references.hinotes.R
 import com.huawei.references.hinotes.data.location.SiteAdapter
+import com.huawei.references.hinotes.ui.base.BaseMapFragment
+import kotlinx.android.synthetic.main.custom_info_window.view.*
 import java.io.UnsupportedEncodingException
 import java.net.URLEncoder
 
-class LocationBottomSheetFragment(lat:Double, lon:Double) : BottomSheetDialogFragment(), OnMapReadyCallback {
+class LocationBottomSheetFragment(lat:Double, lon:Double) : BaseMapFragment() {
     private var hMap: HuaweiMap? = null
     private var mMarker: Marker? = null
     private var lLat:Double = lat
@@ -44,13 +51,9 @@ class LocationBottomSheetFragment(lat:Double, lon:Double) : BottomSheetDialogFra
     ): View? {
         val locationView =
             inflater.inflate(R.layout.note_detail_location_bottom_sheet, container, false)
-        var encodedApiKey: String? = null
-        try {
-            encodedApiKey = URLEncoder.encode(API_KEY, "utf-8")
-        } catch (e: UnsupportedEncodingException) {
-            e.printStackTrace()
-        }
-        searchService = SearchServiceFactory.create(activity, encodedApiKey)
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this.activity)
+        settingsClient = LocationServices.getSettingsClient(this.activity)
 
         val mapView = locationView.findViewById <MapView> (R.id.note_detail_location_mapView)
         var mapViewBundle: Bundle? = null
@@ -61,21 +64,13 @@ class LocationBottomSheetFragment(lat:Double, lon:Double) : BottomSheetDialogFra
         mapView.onCreate(mapViewBundle)
         mapView.getMapAsync(this)
 
-        val siteList = locationView.findViewById <RecyclerView> (R.id.site_list)
-        reSite = siteList
-        getFirstPlace()
         return locationView
     }
 
 
-    @RequiresPermission(allOf = [permission.ACCESS_FINE_LOCATION, permission.ACCESS_WIFI_STATE])
-    override fun onMapReady(huaweiMap: HuaweiMap) {
-        Log.d("onMapReady", huaweiMap.toString())
-        hMap = huaweiMap
-        hMap!!.isMyLocationEnabled = true // Enable the my-location overlay.
-        hMap!!.uiSettings.isCompassEnabled = true
-        hMap!!.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(lLat, lLon), 15F))
-        hMap!!.uiSettings.isMyLocationButtonEnabled = true // Enable the my-location icon.
+    @RequiresPermission(allOf = [Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_WIFI_STATE])
+    override fun onMapReady(huaweiMap: HuaweiMap?) {
+        super.onMapReady(huaweiMap)
     }
 
     /*
@@ -117,54 +112,67 @@ class LocationBottomSheetFragment(lat:Double, lon:Double) : BottomSheetDialogFra
     }
     */
 
+    internal class CustomInfoWindowAdapter(mcontext: Context) :
+        HuaweiMap.InfoWindowAdapter {
+        private val mmContext = mcontext
+        private val mWindow: View = (mcontext as Activity).
+        layoutInflater.inflate(R.layout.custom_info_window, null)
 
-    private fun addMarker(lLat: Double, lLon: Double) {
-        //if (mMarker != null) mMarker!!.remove()
-        val options = MarkerOptions().position(LatLng(lLat, lLon))
-        options.icon(BitmapDescriptorFactory.fromResource(R.drawable.marker))
-        mMarker = hMap!!.addMarker(options)
+        override fun getInfoWindow(marker: Marker?): View {
+            val mInfoWindow: InfoWindowData? = marker?.tag as InfoWindowData?
+
+            mWindow.infoTile.text = mInfoWindow?.siteName
+            mWindow.infoAddress.text = mInfoWindow?.siteAddress
+
+            val txtvdirection = mWindow.findViewById<TextView>(R.id.infoGetDirections)
+            txtvdirection.setOnClickListener {
+
+            }
+
+            return mWindow
+        }
+
+        override fun getInfoContents(marker: Marker): View? {
+            return null
+        }
+
     }
 
-    private fun getFirstPlace() {
-        request = NearbySearchRequest()
-        location = Coordinate(lLat, lLon)
-        request!!.location = location
-        request!!.radius = 6000
-        request!!.poiType = LocationType.GROCERY_OR_SUPERMARKET
-        request!!.language = "en"
-        request!!.pageIndex = 1
-        request!!.pageSize = 10
-        val resultListener: SearchResultListener<NearbySearchResponse> =
-            object : SearchResultListener<NearbySearchResponse> {
-                override fun onSearchResult(results: NearbySearchResponse) {
-                    listSite = results.sites
-                    reSite!!.layoutManager = LinearLayoutManager(activity,LinearLayoutManager.VERTICAL,false)
-                    reSite!!.adapter = SiteAdapter(listSite!!)
-                    if (results.totalCount <= 0 || listSite.isNullOrEmpty()) {
-                        return
-                    }
-                    for (site in listSite!!) {
-                        addMarker(site.location.lat,site.location.lng)
-                        //hMap!!.setInfoWindowAdapter(CustomInfoWindowAdapter(site.name,site.formatAddress.toString()))
-                        Log.i("siteid",
-                            String.format(
-                                "siteId: '%s', name: %s\r\n distance:%f",
-                                site.siteId,
-                                site.name,
-                                site.distance
-                            )
-                        )
-                    }
-                }
 
-                override fun onSearchError(status: SearchStatus) {
-                    Log.i(
-                        "siteid",
-                        "Error : " + status.errorCode + " " + status.errorMessage
-                    )
-                }
-            }
-        searchService!!.nearbySearch(request, resultListener)
+    /*
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        var mapViewBundle =
+            outState.getBundle(MAPVIEW_BUNDLE_KEY)
+        if (mapViewBundle == null) {
+            mapViewBundle = Bundle()
+            outState.putBundle(MAPVIEW_BUNDLE_KEY, mapViewBundle)
+        }
+        mMapView.onSaveInstanceState(mapViewBundle)
+    }
+    */
+
+    private fun setMarkersToMap(placeList : List<Site>){
+        placeList.forEach {
+            val latlng = LatLng(it.location.lat, it.location.lng)
+            val info = InfoWindowData(it.siteId,
+                it.name,
+                it.formatAddress,
+                it.location.lat,
+                it.location.lng
+            )
+            val options = MarkerOptions()
+            options.position(latlng)
+            options.title(it.name)
+            options.draggable(false)
+            options.icon(BitmapDescriptorFactory.fromResource(R.drawable.marker))
+            options.clusterable(false)
+
+            val customInfoWindow = context?.let { ctx -> CustomInfoWindowAdapter(ctx) }
+            hMap!!.setInfoWindowAdapter(customInfoWindow)
+            val marker = hMap!!.addMarker(options)
+            marker.tag = info
+        }
     }
 
     companion object{
